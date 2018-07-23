5f8cb0735d90cd055819611b7311141081571e88
Zaparoli <luizzaparoli07@gmail.com>
2018-07-23 18:54:12 -0300

    Classe Users

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 	
 	require_once("config.php");
 
+	
+	$root = new Users();
+ 
+	$root -> loadbyId(3);
+
+	echo $root;
+
+
+	/*
 	$sql = new Sql();
 
 	$usuarios = $sql -> select("SELECT * FROM USERS");
 
 	echo json_encode($usuarios);
+
+	*/
 
 ?>
